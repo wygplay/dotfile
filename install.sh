81317c0b7aeab00d1ce29f104761bcd0fb8b563c
wygplay <wygplay@gmail.com>
2026-06-19 17:00:03 +0800

    chore: remove init.lua and lazy_setup.lua from dotfile (AstroNvim upgrade will overwrite these)

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@
 ln -s $WORK_DIR/ideavim/.ideavimrc $HOME/.ideavimrc
 ln -s $WORK_DIR/lunarvim/config.lua $HOME/.config/lvim/config.lua
 
-# AstroNvim user config
+# AstroNvim user config (only user-customized files)
 mkdir -p $HOME/.config/nvim/lua/plugins
-ln -sf $WORK_DIR/astrovim/init.lua $HOME/.config/nvim/init.lua
-ln -sf $WORK_DIR/astrovim/lua/lazy_setup.lua $HOME/.config/nvim/lua/lazy_setup.lua
 ln -sf $WORK_DIR/astrovim/lua/community.lua $HOME/.config/nvim/lua/community.lua
 ln -sf $WORK_DIR/astrovim/lua/polish.lua $HOME/.config/nvim/lua/polish.lua
 ln -sf $WORK_DIR/astrovim/lua/plugins/user.lua $HOME/.config/nvim/lua/plugins/user.lua
